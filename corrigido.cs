5fe42e1235939095b23edc395bf48fcb1ef438d5
p2ms <101223900+p2ms@users.noreply.github.com>
2022-04-08 18:06:16 -0300

    Create corrigido.cs

---
+++ corrigido.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int e = 0;
+            int aux;
+            int[] ord = new int[5];
+
+            ord[0] = int.Parse(Console.ReadLine());
+            ord[1] = int.Parse(Console.ReadLine());
+            ord[2] = int.Parse(Console.ReadLine());
+            ord[3] = int.Parse(Console.ReadLine());
+            ord[4] = int.Parse(Console.ReadLine());
+
+            //for (e = 0; e < 5; e++)
+            {//do
+                {
+                    for (i = 0; i < 5; i++)
+                    {
+                        for (e = 0; e < 5; e++)
+                        {
+                            if (ord[i] < ord[e])
+                            {
+                            aux = ord[i];
+                            ord[i] = ord[e];
+                            ord[e] = aux;
+                            }/*else
+                        {
+                            aux = ord[e];
+                            ord[e] = ord[i];
+                            ord[i] = aux;
+                        }*/
+                            
+                        }
+                        
+                    }
+                }
+            }//while (ord[0] != 1);
+            Console.WriteLine("oioi");
+            Console.WriteLine(ord[0]);
+            Console.WriteLine(ord[1]);
+            Console.WriteLine(ord[2]);
+            Console.WriteLine(ord[3]);
+            Console.WriteLine(ord[4]);
+            Console.ReadKey();
+            
+           }
+    }
+}
